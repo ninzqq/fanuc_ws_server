33d78ebde724b81f776d621585c33348114d5ed2
Janne Niinisalo <janne.niinisalo@gmail.com>
2022-02-25 19:39:53 +0200

    Add cors origin: "*"

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
 const axios = require("axios");
 const { join } = require("path");
 
