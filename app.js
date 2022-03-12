4bcd5725cb2630cbc401af3903126c0bf6f44097
Janne Niinisalo <janne.niinisalo@gmail.com>
2022-03-12 10:54:20 +0200

    ready for heroku
    10 fanuc ws ui rotationer

--- app.js
+++ app.js
@@ -1,63 +1,14 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
 const app = express();
-const server = require("http").createServer(app);
 
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "*"
-    }
-});
-
-const axios = require("axios");
-const { join } = require("path");
-
-const PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
+app.use(express.static("node_modules/three/build"));
+app.use(express.static("node_modules/three/examples/jsm/controls"));
+app.use(express.static("node_modules/three/examples/jsm/loaders"));
 
 
-let connections = [];
-io.on("connection", (socket) => {
-    connections[socket.id] = socket;
-
-    socket.on("disconnect", (socket) => {
-        delete connections[socket.id];
-    });
-});
-
-const broadcast_joint_values = (joint_values) => {
-    for (let id in connections){
-        const connection = connections[id];
-        connection.emit("joint_values", joint_values);
-    }
-}; 
-
-const get_joint_values = () => {
-    return axios
-        .get("https://fanuc-robot-http-server.herokuapp.com/")
-        .then((robot_res) => {
-            const regexp = 'Joint   [1-6]: *(-?.*)';
-            let joint_values = [];
-            let matches = robot_res.data.matchAll(regexp);
-            let count = 0;
-            for (const match of matches) {
-                count++;
-                if (count > 6) break;
-                const value = parseFloat(match[1]);
-                joint_values.push(value);
-            }
-            return joint_values;
-        });
-};
-
-const main_loop = () => {
-    get_joint_values().then((joint_values) => {
-        broadcast_joint_values(joint_values);
-        //console.log(joint_values);
-        main_loop();
-    });
-};
-
-main_loop();
-
-
-server.listen(PORT);
-console.log("Listening PORT: ", + PORT);
+app.listen(PORT);
+console.log("Listening port: " + PORT);
---
+++ public/client.js
@@ -0,0 +1,197 @@
+
+import * as THREE from "three";
+import {OrbitControls} from "/OrbitControls.js";
+import {STLLoader} from "/STLLoader.js";
+
+let socket = io("https://auoh-fanuc-ws-server-test.herokuapp.com/", {withCredentials: false});
+
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(75, aspect);
+
+camera.position.z = 4;
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+renderer.setClearColor(0xffffff);
+
+// controls
+let controls = new OrbitControls(camera, renderer.domElement);
+
+// origo red ball
+{
+    let geometry = new THREE.SphereGeometry(0.05);
+    let material = new THREE.MeshBasicMaterial({color: 0xff0000});
+    let mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+}
+
+// light
+{
+    let light = new THREE.DirectionalLight(0xaaaaaa, 1.5);
+    light.position.x = 10;
+    light.position.y = 10;
+    light.position.z = 10;
+    light.lookAt(scene.position);
+    scene.add(light);
+}
+{
+    let light = new THREE.DirectionalLight(0xaaaaaa, 1.5);
+    light.position.x = -10;
+    light.position.y = -10;
+    light.position.z = 10;
+    light.lookAt(scene.position);
+    scene.add(light);
+}
+
+// floor
+{
+    let geometry = new THREE.PlaneBufferGeometry(10, 10);
+    let material = new THREE.MeshPhongMaterial({
+        color: 0xaaaaaa,
+    });
+    let mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+}
+
+let default_material = new THREE.MeshPhongMaterial({
+    color: 0x0000ff,
+});
+
+let dark_material = new THREE.MeshPhongMaterial({
+    color: 0x555555,
+});
+
+let yellow_material = new THREE.MeshPhongMaterial({
+    color: 0xffff00,
+});
+
+const stl_loader = new STLLoader();
+const load_stl = (url) => {
+    return new Promise((resolve) => {
+        stl_loader.load(url, resolve);
+    });
+};
+
+
+// robot
+let joints = [];
+const load_geometries = async () => {
+
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/BASE.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, dark_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J1-1.stl");
+        //let geometry2 = await load_stl("./FANUC_R2000iA165F-STL/J1-2.stl");
+        //geometry.merge(geometry2);
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, yellow_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J2.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, yellow_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J3.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, yellow_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J4.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, yellow_material));
+    }
+    {
+        let geometry = await load_stl("./FANUC_R2000iA165F-STL/J5.stl");
+        geometry.scale(0.001, 0.001, 0.001);
+        joints.push(new THREE.Mesh(geometry, yellow_material));
+    }
+
+};
+
+// Joints origos and rotation axis directions
+// J1: [0, 282, 0] Y
+// J2: [312, 670, -117] Z
+// J3: [268.69, 1744.13, -196.85] Z 
+// J4: [1315.19, 1969.13, 0.15] X
+// J5: [1548.69, 1969.13, 87.15] Z
+// J6: [1763.69, 1969.13, 20.47] X
+
+let offsets = [];
+load_geometries().then(() => {
+
+    // Set the robot's joints' real origo down to floor level
+    joints[1].geometry.translate(0, -0.282, 0);
+    joints[2].geometry.translate(-0.312, -0.670, 0.117);
+    joints[3].geometry.translate(-0.26869, -1.74413, 0.19685);
+    joints[4].geometry.translate(-1.31519, -1.96913, -0.00015);
+    joints[5].geometry.translate(-1.54869, -1.96913, -0.08715);
+    
+    scene.add(joints[0]);
+    joints[0].rotation.set(THREE.Math.degToRad(90), 0, 0);
+
+    offsets.push(new THREE.Group());
+    offsets[0].position.set(0, 0.282, 0);
+    joints[0].add(offsets[0]);
+    offsets[0].add(joints[1]);
+
+    offsets.push(new THREE.Group());
+    offsets[1].position.set(0.312, 0.670-0.282, -0.117);
+    joints[1].add(offsets[1]);
+    offsets[1].add(joints[2]);
+
+    offsets.push(new THREE.Group());
+    offsets[2].position.set(0.26869-0.312, 1.74413-0.670, -0.19685-(-0.117));
+    joints[2].add(offsets[2]);
+    offsets[2].add(joints[3]);
+
+    offsets.push(new THREE.Group());
+    offsets[3].position.set(1.31519-0.26869, 1.96913-1.74413, 0.00015-(-0.19685));
+    joints[3].add(offsets[3]);
+    offsets[3].add(joints[4]);
+
+    offsets.push(new THREE.Group());
+    offsets[4].position.set(1.54869-1.31519, 0, 0.08715-0.00015);
+    joints[4].add(offsets[4]);
+    offsets[4].add(joints[5]);
+
+    socket.on("joint_values", (joint_values) => {
+        //console.log(joint_values);
+
+        joints[1].rotation.set(0, THREE.Math.degToRad(joint_values[0]), 0);
+        joints[2].rotation.set(0, 0, THREE.Math.degToRad(joint_values[1]));
+        joints[3].rotation.set(0, 0, THREE.Math.degToRad(joint_values[2]-joint_values[1]));
+        joints[4].rotation.set(THREE.Math.degToRad(joint_values[3]), 0, 0);
+        joints[5].rotation.set(0, 0, THREE.Math.degToRad(joint_values[4]));
+
+    });
+
+});
+
+//{
+//    load_stl("./FANUC_R2000iA165F-STL/BASE.stl").then((geometry) => {
+//        geometry.scale(0.001, 0.001, 0.001);
+//        let mesh = new THREE.Mesh(geometry, default_material);
+//        scene.add(mesh);
+//    });
+//}
+
+const render = () => {
+    renderer.render(scene, camera);
+
+    controls.update();
+
+    requestAnimationFrame(render);
+};
+
+render();
+
+
