9a6d3715e5a6c16af38059b781c6a6383ad8c2ad
Janne Niinisalo <janne.niinisalo@gmail.com>
2022-03-12 11:29:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require("express");
+<<<<<<< HEAD
+=======
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const axios = require("axios");
+const { join } = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+>>>>>>> parent of 33d78eb (Add cors origin: "*")
 
 const PORT = process.env.PORT || 8080;
 
